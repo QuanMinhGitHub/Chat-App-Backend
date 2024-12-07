05fe209b5fd2528d60d8ee1ae158d7f6d157861c
QuanMinhGitHub <minhquan862004@gmail.com>
2024-12-07 07:54:03 +0700

    Models

--- server.js
+++ server.js
@@ -1,5 +1,26 @@
 const express = require("express")
+const dotenv = require("dotenv")
+const cors = require("cors")
+const { chats } = require("./data")
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+dotenv.config()
 
-app.listen(5000, console.log("Server started on port 5000"))
+app.get("/", (request, response) => {
+    response.send("API is running")
+})
+
+app.get("/api/chat", (request, response) => {
+    response.send(chats)
+})
+
+app.get("/api/chat/:id", (request, response) => {
+    const singleChat = chats.find((c) => c._id === request.params.id)
+    response.send(singleChat)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
